e5ee4dfb49e27e1e94a7a42ceec7221ce20cbc06
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-04-18 20:53:11 +0900

    a

--- main.cpp
+++ main.cpp
@@ -144,19 +144,50 @@ Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
 
 
 //4.逆行列
-Matrix4x4 Inverse(const Matrix4x4& m) {
+Matrix4x4 Inverse(const Matrix4x4& matrix) {
 	Matrix4x4 result;
 
-	float det =
-		matrix.m[0][0] * (matrix.m[1][1] * matrix.m[2][2] - matrix.m[2][1] * matrix.m[1][2]) -
-		matrix.m[0][1] * (matrix.m[1][0] * matrix.m[2][2] - matrix.m[2][0] * matrix.m[1][2]) +
-		matrix.m[0][2] * (matrix.m[1][0] * matrix.m[2][1] - matrix.m[2][0] * matrix.m[1][1]);
+	float det;
+	det =
+		((matrix.m[0][0] * matrix.m[1][1] * matrix.m[2][2] * matrix.m[3][3]) +
+			(matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][3] * matrix.m[3][1]) +
+			(matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][1] * matrix.m[3][2]) +
+
+			(matrix.m[0][1] * matrix.m[1][0] * matrix.m[2][3] * matrix.m[3][2]) +
+			(matrix.m[0][1] * matrix.m[1][2] * matrix.m[2][0] * matrix.m[3][3]) +
+			(matrix.m[0][1] * matrix.m[1][3] * matrix.m[2][2] * matrix.m[3][0]) +
+
+			(matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][1] * matrix.m[3][3]) +
+			(matrix.m[0][2] * matrix.m[1][1] * matrix.m[2][3] * matrix.m[3][0]) +
+			(matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][0] * matrix.m[3][1]) +
+
+			(matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][2] * matrix.m[3][1]) +
+			(matrix.m[0][3] * matrix.m[1][1] * matrix.m[2][0] * matrix.m[3][2]) +
+			(matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][1] * matrix.m[3][0]) -
 
 
-	Matrix3x3 result;
+			(matrix.m[0][0] * matrix.m[1][1] * matrix.m[2][3] * matrix.m[3][2]) -
+			(matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][1] * matrix.m[3][3]) -
+			(matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][2] * matrix.m[3][1]) -
+
+			(matrix.m[0][1] * matrix.m[1][0] * matrix.m[2][2] * matrix.m[3][3]) -
+			(matrix.m[0][1] * matrix.m[1][2] * matrix.m[2][3] * matrix.m[3][0]) -
+			(matrix.m[0][1] * matrix.m[1][3] * matrix.m[2][0] * matrix.m[3][2]) -
+
+			(matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][3] * matrix.m[3][1]) -
+			(matrix.m[0][2] * matrix.m[1][1] * matrix.m[2][0] * matrix.m[3][3]) -
+			(matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][1] * matrix.m[3][0]) -
+
+			(matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][1] * matrix.m[3][2]) -
+			(matrix.m[0][3] * matrix.m[1][1] * matrix.m[2][2] * matrix.m[3][0]) -
+			(matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][0] * matrix.m[3][1]));
+
+
+	Matrix4x4 result;
 	result.m[0][0] = (matrix.m[1][1] * matrix.m[2][2] - matrix.m[2][1] * matrix.m[1][2]) / det;
 	result.m[0][1] = (matrix.m[0][2] * matrix.m[2][1] - matrix.m[2][2] * matrix.m[0][1]) / det;
 	result.m[0][2] = (matrix.m[0][1] * matrix.m[1][2] - matrix.m[1][1] * matrix.m[0][2]) / det;
+	result.m[0][3] = (matrix.m[0][1] * matrix.m[1][2] - matrix.m[1][1] * matrix.m[0][2]) / det;
 
 	return result;
 };
