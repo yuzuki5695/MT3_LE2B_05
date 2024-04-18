2b9e19f17f443a427c42d752823345704a986226
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-04-18 19:21:51 +0900

    a

--- main.cpp
+++ main.cpp
@@ -147,6 +147,17 @@ Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
 Matrix4x4 Inverse(const Matrix4x4& m) {
 	Matrix4x4 result;
 
+	float det =
+		matrix.m[0][0] * (matrix.m[1][1] * matrix.m[2][2] - matrix.m[2][1] * matrix.m[1][2]) -
+		matrix.m[0][1] * (matrix.m[1][0] * matrix.m[2][2] - matrix.m[2][0] * matrix.m[1][2]) +
+		matrix.m[0][2] * (matrix.m[1][0] * matrix.m[2][1] - matrix.m[2][0] * matrix.m[1][1]);
+
+
+	Matrix3x3 result;
+	result.m[0][0] = (matrix.m[1][1] * matrix.m[2][2] - matrix.m[2][1] * matrix.m[1][2]) / det;
+	result.m[0][1] = (matrix.m[0][2] * matrix.m[2][1] - matrix.m[2][2] * matrix.m[0][1]) / det;
+	result.m[0][2] = (matrix.m[0][1] * matrix.m[1][2] - matrix.m[1][1] * matrix.m[0][2]) / det;
+
 	return result;
 };
 
