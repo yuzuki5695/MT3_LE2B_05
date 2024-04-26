384820173f2b8bf3e532cedddec6d5b2779d9f8b
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-04-26 10:01:23 +0900

    MT3_01_00

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip
 	Matrix4x4 result{};
 
 
-	result.m[0][0] = (1.0f / aspectRatio) * tanf(fovY/ 2);result.m[0][1] = 0.0f; result.m[0][2] = 0.0f; result.m[0][3] = 0.0f;
+	result.m[0][0] = (1.0f / aspectRatio) / tanf(fovY/ 2);result.m[0][1] = 0.0f; result.m[0][2] = 0.0f; result.m[0][3] = 0.0f;
 	result.m[1][0] = 0.0f; result.m[1][1] = (1.0f /tanf(fovY / 2)); result.m[1][2] = 0.0f; result.m[1][3] = 0.0f;
 	result.m[2][0] = 0.0f; result.m[2][1] = 0.0f; result.m[2][2] = farclip /(farclip - nearClip) ; result.m[2][3] = 1.0f;
 	result.m[3][0] = 0.0f; result.m[3][1] = 0.0f; result.m[3][2] = (-nearClip * farclip) / (farclip - nearClip ); result.m[3][3] = 0.0f;
