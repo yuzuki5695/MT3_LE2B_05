a7db2d01cc3b6247ed8f71ef6381c8c22c851980
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-07-03 14:13:07 +0900

    MT3_01_02

--- main.cpp
+++ main.cpp
@@ -23,60 +23,52 @@ struct Matrix4x4 {
 
 
 struct Sphere {
-	Vector3 center;
-	float radius;  
+	Vector3 center; //!< 中心点
+	float radius; //!< 半径
 };
 
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+//加算
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = (v1.x + v2.x);
+	result.y = (v1.y + v2.y);
+	result.z = (v1.z + v2.z);
+	return result;
+};
 
-	for (int row = 0; row < 4; ++row) {
-		for (int column = 0; column < 4; ++column) {
-			Novice::ScreenPrintf(
-				x + column * Kcolumnwidth, y + row * KRowHeight, "%6.02f", matrix.m[row][column]);
-		}
-	}
 
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
-//1.透視投影行列　
-Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farclip) {
-	Matrix4x4 result{};
-	float f = 1.0f / tan(fovY * 0.5f * float(M_PI) / 180.0f);
-	float ZRange = nearClip - farclip;
-	result.m[0][0] = f / aspectRatio; result.m[0][1] = 0.0f; result.m[0][2] = 0.0f; result.m[0][3] = 0.0f;
-	result.m[1][0] = 0.0f; result.m[1][1] = f; result.m[1][2] = 0.0f; result.m[1][3] = 0.0f;
-	result.m[2][0] = 0.0f; result.m[2][1] = 0.0f; result.m[2][2] = (farclip / nearClip) / ZRange; result.m[2][3] = 1.0f;
-	result.m[3][0] = 0.0f; result.m[3][1] = 0.0f; result.m[3][2] = 1.0f * farclip * nearClip / ZRange; result.m[3][3] = 0.0f;
+//長さ（ノルム）
+float Length(const Vector3& v) {
+	return sqrtf(powf(v.x, 2) + powf(v.y, 2) + powf(v.z, 2));
+};
 
+// 球と球の当たり判定
+bool  IsCollision(const Sphere& sphere1, const Sphere& sphere2) {
+	float distance = Length(Add(sphere2.center, Vector3{ -sphere1.center.x, -sphere1.center.y, -sphere1.center.z }));
+	// 半径の合計よりも短ければ衝突
+	return distance <= (sphere1.radius + sphere2.radius);
+}
+
+
+//正規化
+Vector3  Normalize(const Vector3& v) {
+	Vector3 result{};
+
+	float length = Length(v);
+
+	if (length != 0.0) {
+		result.x = v.x / length;
+		result.y = v.y / length;
+		result.z = v.z / length;
+	}
 
 	return result;
 };
-
-
-//2.正射影行列
-Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float facDlip) {
-	Matrix4x4 result{};
-
-	result.m[0][0] = 2.0f / (right - left); result.m[0][1] = 0; result.m[0][2] = 0; result.m[0][3] = 0;
-	result.m[1][0] = 0; result.m[1][1] = 2.0f / (top - bottom); result.m[1][2] = 0; result.m[1][3] = 0;
-	result.m[2][0] = 0; result.m[2][1] = 0.0f; result.m[2][2] = 1.0f / (facDlip - nearClip); result.m[2][3] = 0;
-	result.m[3][0] = (left + right) / (left - right); result.m[3][1] = (top + bottom) / (bottom - top); result.m[3][2] = nearClip / (nearClip - facDlip); result.m[3][3] = 1.0f;
-
-	return result;
-};
-
-
-//3.ビューボート変換行列
-Matrix4x4 MakeViewportMatrix(float left, float top, float  width, float height, float minDepth, float maxDepth) {
-	Matrix4x4 result{};
-
-	result.m[0][0] = (width / 2.0f); result.m[0][1] = 0.0f; result.m[0][2] = 0.0f; result.m[0][3] = 0.0f;
-	result.m[1][0] = 0.0f; result.m[1][1] = -(height / 2.0f); result.m[1][2] = 0.0f; result.m[1][3] = 0.0f;
-	result.m[2][0] = 0.0f; result.m[2][1] = 0.0f; result.m[2][2] = (maxDepth - minDepth); result.m[2][3] = 0.0f;
-	result.m[3][0] = left + (width / 2); result.m[3][1] = top + (height / 2.0f); result.m[3][2] = minDepth; result.m[3][3] = 1.0f;
-	return result;
-};
-
 
 // 1. X軸回転行列
 Matrix4x4 MakeRotateXMatrix(float radian) {
@@ -140,11 +132,8 @@ Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
 	return result;
 };
 
-
-
-
 //5.3次元アフィン変換
-Matrix4x4 MakeAftineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
 	Matrix4x4 result;
 	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
 	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
@@ -159,9 +148,23 @@ Matrix4x4 MakeAftineMatrix(const Vector3& scale, const Vector3& rotate, const Ve
 	return result;
 };
 
+//3次元ベクトルを同次座標として変換する 
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];//PosX
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];//PosY
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];//PosZ
 
-//4.逆行列
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];;
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+};
+
+// 逆行列
 Matrix4x4 Inverse(const Matrix4x4& matrix) {
 
 	float det
@@ -195,9 +198,6 @@ Matrix4x4 Inverse(const Matrix4x4& matrix) {
 	return result;
 };
 
-
-
-
 //クロス積
 Vector3 Cross(const Vector3& v1, const Vector3& v2) {
 	Vector3 result;
@@ -209,22 +209,69 @@ Vector3 Cross(const Vector3& v1, const Vector3& v2) {
 };
 
 
-//3次元ベクトルを同次座標として変換する 
-Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
+//1.透視投影行列　
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farclip) {
+	Matrix4x4 result{};
 
-	Vector3 result;
-	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];//PosX
-	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];//PosY
-	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];//PosZ
 
-	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];;
-	assert(w != 0.0f);
-	result.x /= w;
-	result.y /= w;
-	result.z /= w;
+	result.m[0][0] = (1.0f / aspectRatio) / tanf(fovY / 2); result.m[0][1] = 0.0f; result.m[0][2] = 0.0f; result.m[0][3] = 0.0f;
+	result.m[1][0] = 0.0f; result.m[1][1] = (1.0f / tanf(fovY / 2)); result.m[1][2] = 0.0f; result.m[1][3] = 0.0f;
+	result.m[2][0] = 0.0f; result.m[2][1] = 0.0f; result.m[2][2] = farclip / (farclip - nearClip); result.m[2][3] = 1.0f;
+	result.m[3][0] = 0.0f; result.m[3][1] = 0.0f; result.m[3][2] = (-nearClip * farclip) / (farclip - nearClip); result.m[3][3] = 0.0f;
+
+
 	return result;
 };
 
+//2.正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float facDlip) {
+	Matrix4x4 result{};
+
+	result.m[0][0] = 2.0f / (right - left); result.m[0][1] = 0; result.m[0][2] = 0; result.m[0][3] = 0;
+	result.m[1][0] = 0; result.m[1][1] = 2.0f / (top - bottom); result.m[1][2] = 0; result.m[1][3] = 0;
+	result.m[2][0] = 0; result.m[2][1] = 0.0f; result.m[2][2] = 1.0f / (facDlip - nearClip); result.m[2][3] = 0;
+	result.m[3][0] = (left + right) / (left - right); result.m[3][1] = (top + bottom) / (bottom - top); result.m[3][2] = nearClip / (nearClip - facDlip); result.m[3][3] = 1.0f;
+
+	return result;
+};
+
+//3.ビューボート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float  width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result{};
+
+	result.m[0][0] = (width / 2.0f); result.m[0][1] = 0.0f; result.m[0][2] = 0.0f; result.m[0][3] = 0.0f;
+	result.m[1][0] = 0.0f; result.m[1][1] = -(height / 2.0f); result.m[1][2] = 0.0f; result.m[1][3] = 0.0f;
+	result.m[2][0] = 0.0f; result.m[2][1] = 0.0f; result.m[2][2] = (maxDepth - minDepth); result.m[2][3] = 0.0f;
+	result.m[3][0] = left + (width / 2); result.m[3][1] = top + (height / 2.0f); result.m[3][2] = minDepth; result.m[3][3] = 1.0f;
+	return result;
+};
+
+void DrawGrid(const Matrix4x4& viewProiectionMatrix, const Matrix4x4& ViewportMatrix) {
+	const float KGridHalfwidth = 2.0f;
+	const uint32_t KSubdivision = 10;
+	const float KGridEvery = (KGridHalfwidth * 2.0f) / float(KSubdivision);
+
+	for (uint32_t xIndex = 0; xIndex <= KSubdivision; xIndex++) {
+		float posX = -KGridHalfwidth + xIndex * KGridEvery;
+
+		Vector3 startPointX(posX, 0.0f, -KGridHalfwidth);
+		Vector3 endPointX(posX, 0.0f, KGridHalfwidth);
+
+		startPointX = Transform(startPointX, Multiply(viewProiectionMatrix, ViewportMatrix));
+		endPointX = Transform(endPointX, Multiply(viewProiectionMatrix, ViewportMatrix));
+		Novice::DrawLine((int)startPointX.x, (int)startPointX.y, (int)endPointX.x, (int)endPointX.y, 0x6F6F6FFF);
+	}
+	for (uint32_t zIndex = 0; zIndex <= KSubdivision; zIndex++) {
+		float posZ = -KGridHalfwidth + KGridEvery * zIndex;
+
+		Vector3 startPointZ = { -KGridHalfwidth, 0.0f, posZ };
+		Vector3 endPointZ = { KGridHalfwidth, 0.0f, posZ };
+		startPointZ = Transform(startPointZ, Multiply(viewProiectionMatrix, ViewportMatrix));
+		endPointZ = Transform(endPointZ, Multiply(viewProiectionMatrix, ViewportMatrix));
+		Novice::DrawLine((int)startPointZ.x, (int)startPointZ.y, (int)endPointZ.x, (int)endPointZ.y, 0x6F6F6FFF);
+	}
+
+}
 
 static void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
 {
@@ -283,35 +330,9 @@ static void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatr
 	}
 }
 
-
-
-
-void DrawGrid(const Matrix4x4& viewProiectionMatrix, const Matrix4x4& ViewportMatrix) {
-	const float KGridHalfwidth = 2.0f;
-	const uint32_t KSubdivision = 10;
-	const float KGridEvery = (KGridHalfwidth * 2.0f) / float(KSubdivision);
-
-	for (uint32_t xIndex = 0; xIndex <= KSubdivision; xIndex++) {
-		float posX = -KGridHalfwidth + xIndex * KGridEvery;
-
-		Vector3 startPointX(posX, 0.0f, -KGridHalfwidth);
-		Vector3 endPointX(posX, 0.0f, KGridHalfwidth);
-
-		startPointX = Transform(startPointX, Multiply(viewProiectionMatrix, ViewportMatrix));
-		endPointX = Transform(endPointX, Multiply(viewProiectionMatrix, ViewportMatrix));
-		Novice::DrawLine((int)startPointX.x, (int)startPointX.y, (int)endPointX.x, (int)endPointX.y, 0x6F6F6FFF);
-	}
-	for (uint32_t zIndex = 0; zIndex <= KSubdivision; zIndex++) {
-		float posZ = -KGridHalfwidth + KGridEvery * zIndex;
-
-		Vector3 startPointZ = { -KGridHalfwidth, 0.0f, posZ };
-		Vector3 endPointZ = { KGridHalfwidth, 0.0f, posZ };
-		startPointZ = Transform(startPointZ, Multiply(viewProiectionMatrix, ViewportMatrix));
-		endPointZ = Transform(endPointZ, Multiply(viewProiectionMatrix, ViewportMatrix));
-		Novice::DrawLine((int)startPointZ.x, (int)startPointZ.y, (int)endPointZ.x, (int)endPointZ.y, 0x6F6F6FFF);
-	}
-
-}
+Vector3 Project(const Vector3& v1, const Vector3& v2) {
+	return (Dot(v1, v2) / powf(Length(v2), 2), v2);
+};
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -320,17 +341,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	Vector3 point{ -1.5f,0.6f,0.6f };
+
+	Sphere sphere1{};
+	Sphere sphere2{};
+	sphere1.radius = 0.5f;
+	sphere2.radius = 0.3f;
+	sphere2.center.x = 1.0f;
+	Vector3 rotate = {};
+	Vector3 translate = {};
+
+	Vector3 camaraTranslate = { 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate = { 0.26f,0.0f,0.0f };
+
+	bool fige = false;
+
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
-
-	Vector3  cameraTranslate{ 0.0f,1.9f,-6.49f };
-	Vector3  cameraRotare{ 0.26f,0.0f,0.0f };
-	Vector3 cameraPosition{};
-
-	Sphere sphere{};
-
-	sphere.radius = 0.5f;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -341,24 +369,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
+
 		///
 		/// ↓更新処理ここから
 		///
 
-		Matrix4x4 worldMatrix = MakeAftineMatrix({ 1.0f,1.0f,1.0f }, cameraRotare, cameraTranslate);
-		Matrix4x4 cameraMatrix = MakeAftineMatrix({ 1.0f, 1.0f, 1.0f }, { 0.0f,0.0f,0.0f }, cameraPosition);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, { 0.0f,0.0f,0.0f });
+		Matrix4x4 viewWorldMatrix = Inverse(worldMatrix);
+
+		Matrix4x4 cameraMatrxi = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, camaraTranslate);
+		Matrix4x4 viewCameraMatrix = Inverse(cameraMatrxi);
 
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		Matrix4x4 ViewProjectionMatrix = Multiply(viewMatrix, Multiply(viewMatrix, projectionMatrix));
+		Matrix4x4 ViewProjectionMatrix = Multiply(viewWorldMatrix, Multiply(viewCameraMatrix, projectionMatrix));
 		Matrix4x4 ViewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 
-		ImGui::Begin("Window");
-		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
-		ImGui::DragFloat3("CameraRotare", &cameraRotare.x, 0.01f);
-		ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
-		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);
+		// 球と球の当たり判定
+		if (IsCollision(sphere1, sphere2)) {
+			// 球同士が当たったら
+			fige = true;
+		} else {
+			// 球同士が当たらなかったら
+			fige = false;
+		}
 
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("sphere[1]", &sphere1.center.x, 0.01f);
+		ImGui::DragFloat("sphere[1]", &sphere1.radius, 0.01f);
+		ImGui::DragFloat3("sphere[2]", &sphere2.center.x, 0.01f);
+		ImGui::DragFloat("sphere[2]", &sphere2.radius, 0.01f);
 
 		///
 		/// ↑更新処理ここまで
@@ -367,12 +406,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-	
-		
-		DrawGrid(viewMatrix, viewMatrix);
 
-		DrawSphere(sphere, viewMatrix, viewMatrix, 0XFFFFFFFF);
+		DrawGrid(ViewProjectionMatrix, ViewportMatrix);
 
+
+		if (fige == true) {
+			DrawSphere(sphere1, ViewProjectionMatrix, ViewportMatrix, RED);	
+			DrawSphere(sphere2, ViewProjectionMatrix, ViewportMatrix, WHITE);
+		} else if (fige == false) {
+			DrawSphere(sphere1, ViewProjectionMatrix, ViewportMatrix, WHITE);
+			DrawSphere(sphere2, ViewProjectionMatrix, ViewportMatrix, WHITE);
+		}
 
 		ImGui::End();
 
