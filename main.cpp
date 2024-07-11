280742a72c64c5b66b1a6a9bd04a0677454d51cc
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-07-11 14:12:45 +0900

    MT3_02_03

--- main.cpp
+++ main.cpp
@@ -51,8 +51,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 camaraTranslate = { 0.0f,1.9f,-6.49f };
 	Vector3 cameraRotate = { 0.26f,0.0f,0.0f };
 
-
-
 	Segment segment{};
 	segment.diff.x = 1.2f;
 	segment.diff.y = 0.5f;
