360fee28d6f3d5573ed521e02b8d7d421b5615dd
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-07-19 15:17:57 +0900

    MT3_03_00

--- main.cpp
+++ main.cpp
@@ -23,15 +23,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 camaraTranslate = { 0.0f,1.9f,-6.49f };
 	Vector3 cameraRotate = { 0.26f,0.0f,0.0f };
 
-	AABB aabb;
-	aabb.min = { -0.5f,-0.5f,-0.5f };
-	aabb.max = { 0.5f,0.5f,0.5f };
-
-	Segment segment{};
-	segment.origin ={ -0.7f,0.3f,0.0f};
-
-
-	bool fige = false;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -60,26 +51,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 		Matrix4x4 ViewProjectionMatrix = Multiply(viewWorldMatrix, Multiply(viewCameraMatrix, projectionMatrix));
 		Matrix4x4 ViewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-		
-		Vector3 start = Transform(Transform(segment.origin, ViewProjectionMatrix), ViewportMatrix);
-		Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), ViewProjectionMatrix), ViewportMatrix);
 
-		
-		// AABBと線の衝突判定
-		if (IsCollision(aabb,segment)) {
-			// 衝突したら
-			fige = true;
-		} else {
-			// 衝突しなかったら
-			fige = false;
-		}
+
 
 		ImGui::Begin("Window");
-		ImGui::DragFloat3("AABB.Min", &aabb.min.x, 0.01f);
-		ImGui::DragFloat3("AABB.Max", &aabb.max.x, 0.01f);
-		ImGui::DragFloat3("Segment.segment", &segment.origin.x, 0.01f);
-		ImGui::DragFloat3("Segment.diff", &segment.diff.x, 0.01f);
-		ImGui::Checkbox("fige",&fige);
+		//ImGui::DragFloat3("AABB.Min", &aabb.min.x, 0.01f);
+
 
 		///
 		/// ↑更新処理ここまで
@@ -90,14 +67,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		DrawGrid(ViewProjectionMatrix, ViewportMatrix);
-
-		Novice::DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, WHITE);
-
-		if (fige == true) {
-			DrawAABB(aabb, ViewProjectionMatrix, ViewportMatrix, RED);
-		} else if (fige == false) {
-			DrawAABB(aabb, ViewProjectionMatrix, ViewportMatrix, WHITE);
-		}
 
 		ImGui::End();
 
