8e5a793400623c87769507e86d75785fe1e33b79
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-07-19 12:37:01 +0900

    MT3_02_05

--- Draw.h
+++ Draw.h
@@ -117,9 +117,9 @@ void DrawAABB(const AABB& aabb, const Matrix4x4& viewProiectionMatrix, const Mat
 // AABB同士の衝突判定
 bool  IsCollision(const AABB& aabb1, const  AABB& aabb2) {
 
-	if ( (aabb1.min.x<= aabb2.max.x && aabb1.max.x >= aabb2.min.x) && // X
-		(aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) && // Y
-		(aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z)    // Z 
+	if ( (aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x) &&  // X
+	 	 (aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) && // Y
+		 (aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z)    // Z 
 		) {
 		return true;
 	}
