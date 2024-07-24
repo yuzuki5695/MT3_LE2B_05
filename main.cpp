0150469061927c982f3c2b68b70df25d9a224737
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-07-24 16:18:56 +0900

    MT3_04_01

--- main.cpp
+++ main.cpp
@@ -34,12 +34,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Ball ball{};
 	ball.position = { 1.2f,0.0f,0.0f };
 	ball.mass = 2.0f;
-	ball.radius = 0.05f;
+	ball.radius = 0.08f;
 	ball.color = BLUE;
 
 	float deltaTime = 1.0f / 60.0f;
 
 	bool start = false;
+	float angularVelocity = 3.14f;
+	float angle = 0.0f;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -72,58 +74,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		sphere.center = { ball.position };
 		sphere.radius = ball.radius;
 
-		Vector3 diff = { 
-		ball.position.x - spring.anchor.x,
-		ball.position.y - spring.anchor.y, 
-		ball.position.z - spring.anchor.z 
-		};
-
 		if (start) {
-			float length = Length(diff);
-			if (length != 0.0f) {
-				Vector3 direction = Normalize(diff);
-				Vector3 restPosition = {
-				 spring.anchor.x + direction.x * spring.naturalLength,
-				 spring.anchor.y + direction.y * spring.naturalLength,
-				 spring.anchor.z + direction.z * spring.naturalLength,
-				};
-				Vector3 displacement = {
-					length * (ball.position.x - restPosition.x),
-					length * (ball.position.y - restPosition.y),
-					length * (ball.position.z - restPosition.z),
-				};
+			angle += angularVelocity * deltaTime;
 
-				Vector3 restoringForce = {
-					-spring.stiffness * displacement.x,
-					-spring.stiffness * displacement.y,
-					-spring.stiffness * displacement.z
-				};
+			// 円運動の位置を計算
+			ball.position.x = sphere.center.x + std::cos(angle) * ball.radius;
+			ball.position.y = sphere.center.y + std::sin(angle) * ball.radius;
+			ball.position.z = sphere.center.z;
 
-				// 減衰抵抗を計算する
-				Vector3 dampingForce = {
-					-spring.dampingCoefficient * ball.velocity.x,
-					-spring.dampingCoefficient * ball.velocity.y,
-					-spring.dampingCoefficient * ball.velocity.z
-				};
-				//	減衰抵抗も加味して、物体にかかる力を決定する
-				Vector3 force = {
-					restoringForce.x + dampingForce.x,
-					restoringForce.y + dampingForce.y,
-					restoringForce.z + dampingForce.z
-				};
+			// 速度の計算
+			ball.velocity.x = -ball.radius * std::sin(angle) * ball.radius;
+			ball.velocity.y = ball.radius *  std::cos(angle) * ball.radius;
+			ball.velocity.z = 0.0f;
 
-				ball.acceleration.x = force.x / ball.mass;
-				ball.acceleration.y = force.y / ball.mass;
-				ball.acceleration.z = force.z / ball.mass;
-
-				ball.velocity.x += ball.acceleration.x * deltaTime;
-				ball.velocity.y += ball.acceleration.y * deltaTime;
-				ball.velocity.z += ball.acceleration.z * deltaTime;
-
-				ball.position.x += ball.velocity.x * deltaTime;
-				ball.position.y += ball.velocity.y * deltaTime;
-				ball.position.z += ball.velocity.z * deltaTime;
-			}
+			// 加速度の計算
+			ball.acceleration.x = -angularVelocity * angularVelocity * ball.position.x;
+			ball.acceleration.y = -angularVelocity * angularVelocity * ball.position.y;
+			ball.acceleration.z = 0.0f;
 		}
 
 		if (!start){
@@ -135,8 +102,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			ball.position = { 1.2f,0.0f,0.0f };
 			ball.mass = 2.0f;
 			ball.radius = 0.05f;
-			ball.color = BLUE;
+			ball.color = WHITE;
 		}
+
 
 		ImGui::Begin("Window");	
 		ImGui::Checkbox("Start", &start);
